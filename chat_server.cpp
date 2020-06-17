ea24a3210332c4daace9b2735f35e7c32f11a517
wuwuz <1273163325@qq.com>
2020-06-17 16:25:19 +0800

    remove dependency

--- chat_server.cpp
+++ chat_server.cpp
@@ -8,9 +8,9 @@
 #include <iostream>
 #include <list>
 #include <set>
-#include <boost/bind.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
+//#include <boost/bind.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/enable_shared_from_this.hpp>
 //#include <boost/asio.hpp>
 #include "chat_message.hpp"
 
